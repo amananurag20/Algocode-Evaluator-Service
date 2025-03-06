06d100413bfc9ad5460544af3c72c369f820e3a1
aman anurag <amananurag.20@gmail.com>
2025-03-06 21:36:55 +0530

    Bull mq based redis queue infra up and running

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
 import express, { Express } from "express";
 
 import serverConfig from "./config/serverConfig";
+import sampleQueueProducer from "./producers/sampleQueueProducer";
 import apiRouter from "./routes";
-
+import SampleWorker from "./workers/SampleWorker";
 
 const app: Express = express();
 
-app.use('/api', apiRouter);
+app.use("/api", apiRouter);
 
 app.listen(serverConfig.PORT, () => {
   console.log(`Server started at *:${serverConfig.PORT}`);
+
+  SampleWorker("SampleQueue");
+
+  sampleQueueProducer("SampleJob", {
+    name: "Sanket",
+    company: "Microsoft",
+    position: "SDE 2 L61",
+    locatiion: "Remote | BLR | Noida",
+  });
 });
