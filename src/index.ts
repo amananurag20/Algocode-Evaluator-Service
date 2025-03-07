f9eabb7cce3dd6c7f236eba5e0b278dc9cb784cb
aman anurag <amananurag.20@gmail.com>
2025-03-07 15:51:03 +0530

    Created docker container for python and able to run and get logs from it

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, { Express } from "express";
 
 import bullBoardAdapter from "./config/bullBoardConfig";
 import serverConfig from "./config/serverConfig";
+import runPython from "./containers/runPythonDocker";
 import apiRouter from "./routes";
 import SampleWorker from "./workers/SampleWorker";
 
@@ -22,4 +23,16 @@ app.listen(serverConfig.PORT, () => {
   );
 
   SampleWorker("SampleQueue");
+
+  const code = `x = input()
+y = input()
+print("value of x is", x)
+print("value of y is", y)
+`;
+
+  const inputCase = `100
+200
+`;
+
+  runPython(code, inputCase);
 });
